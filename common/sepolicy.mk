49dc5be99ab4ac05b6b87e1e43c9366648148016
Bartłomiej Rudecki <ejbtrd@gmail.com>
2022-04-22 17:57:50 +0200

    sepolicy: Initialize sepha

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,6 +1,6 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Lineage
+# inherit from sepha
 #
 
 ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
@@ -10,25 +10,25 @@ endif
 endif
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/public
+    device/sepha/sepolicy/common/public
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/private
+    device/sepha/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/dynamic \
-    device/lineage/sepolicy/common/system
+    device/sepha/sepolicy/common/dynamic \
+    device/sepha/sepolicy/common/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/dynamic \
-    device/lineage/sepolicy/common/vendor
+    device/sepha/sepolicy/common/dynamic \
+    device/sepha/sepolicy/common/vendor
 endif
 
 # Selectively include legacy rules defined by the products
--include device/lineage/sepolicy/legacy-common/sepolicy.mk
+-include device/sepha/sepolicy/legacy-common/sepolicy.mk
 
 # Include atv rules on atv product
 ifeq ($(PRODUCT_IS_ATV), true)
-include device/lineage/sepolicy/atv/sepolicy.mk
+include device/sepha/sepolicy/atv/sepolicy.mk
 endif
